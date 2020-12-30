3822a4ece6b3fc06d3f15e892c7d345fa0ef9eb9
taoyuchuan <taoyuchuan@gmail.com>
2020-12-29 22:38:14 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,35 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+const BUY_CAKE = 'BUY_CAKE'
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: 'First redux action'
+  }
+}
+
+// (previousState, action) => newState
+const initialState = {
+  numberOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case BUY_CAKE: return {
+      ...state,
+      numberOfCakes: state.numberOfCakes - 1,
+    }
+
+    default: return state
+  }
+}
+
+const store = createStore(reducer)
+console.log('initial state', store.getState())
+const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
