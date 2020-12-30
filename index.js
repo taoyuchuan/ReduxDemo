9add06a3169c39e6e10572481ce0ff2fed0cd7a8
taoyuchuan <taoyuchuan@gmail.com>
2020-12-29 23:03:35 -0500

    Add multiple reducers

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const redux = require('redux')
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICECREAM = 'BUY_ICECREAM'
 
 function buyCake() {
   return {
@@ -10,12 +12,23 @@ function buyCake() {
   }
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM
+  }
+}
+
 // (previousState, action) => newState
-const initialState = {
+
+const initialCakeState = {
   numberOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numberOfIceCreams: 20
+}
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch(action.type) {
     case BUY_CAKE: return {
       ...state,
@@ -26,10 +39,27 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-const store = createStore(reducer)
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch(action.type) {
+    case BUY_ICECREAM: return {
+      ...state,
+      numberOfIceCreams: state.numberOfIceCreams - 1,
+    }
+
+    default: return state
+  }
+}
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer
+})
+const store = createStore(rootReducer)
 console.log('initial state', store.getState())
 const unsubscribe = store.subscribe(() => console.log('updated state', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 unsubscribe()
