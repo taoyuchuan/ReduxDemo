65bc96e596536c249749aacf5e6c0a9c81036b78
taoyuchuan <taoyuchuan@gmail.com>
2021-01-06 14:17:29 -0500

    Implement add items functions

--- shop.js
+++ shop.js
@@ -8,17 +8,34 @@ const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAM = 'BUY_ICECREAM'
+const ADD_CAKE = 'ADD_CAKE'
+const ADD_ICECREAM = 'ADD_ICECREAM'
 
 function buyCake() {
   return {
     type: BUY_CAKE,
-    info: 'First redux action'
+    info: 'Buy a cake from store'
+  }
+}
+
+function addCake() {
+  return {
+    type: ADD_CAKE,
+    info: 'Add a cake to store'
   }
 }
 
 function buyIceCream() {
   return {
-    type: BUY_ICECREAM
+    type: BUY_ICECREAM,
+    info: 'Buy an ice cream from store'
+  }
+}
+
+function addIceCream() {
+  return {
+    type: ADD_ICECREAM,
+    info: 'ADD an ice cream to store'
   }
 }
 
@@ -39,6 +56,11 @@ const cakeReducer = (state = initialCakeState, action) => {
       numberOfCakes: state.numberOfCakes - 1,
     }
 
+    case ADD_CAKE: return {
+      ...state,
+      numberOfCakes: state.numberOfCakes + 1,
+    }
+
     default: return state
   }
 }
@@ -48,6 +70,11 @@ const iceCreamReducer = (state = initialIceCreamState, action) => {
     case BUY_ICECREAM: return {
       ...state,
       numberOfIceCreams: state.numberOfIceCreams - 1,
+    }
+
+    case ADD_ICECREAM: return {
+      ...state,
+      numberOfIceCreams: state.numberOfIceCreams + 1
     }
 
     default: return state
@@ -64,6 +91,9 @@ const unsubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(addCake())
+store.dispatch(addCake())
 store.dispatch(buyIceCream())
 store.dispatch(buyIceCream())
+store.dispatch(addIceCream())
 unsubscribe()
